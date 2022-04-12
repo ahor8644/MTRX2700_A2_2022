6f9f5ce2ace02ab739ab94b999125ad39c5bc15b
ahor8644 <101688580+ahor8644@users.noreply.github.com>
2022-04-13 00:27:49 +1000

    Update timer.c

--- MTRX2700_Assignment2/Sources/timer.c
+++ MTRX2700_Assignment2/Sources/timer.c
@@ -3,9 +3,14 @@
 
 
 
-
 //initialising timer and output compare for CH7 and CH5
 void init_timer(void){
+
+  /*
+    This function enables and sets up the timer with a desired prescaler,
+    output compare bits, fast flag clear and PORTT output (for functions
+    like controlling the speaker). 
+  */
   
   TSCR1_TEN = 1;      //enable the timer 
   TSCR1_TFFCA = 1;    //set the fast flag clear (so reading TCNT to re-enables interrupt)
@@ -19,9 +24,6 @@ void init_timer(void){
   TIOS_IOS7 = 1;      // Output compare for ch7
   TIOS_IOS5 = 1;      // Output compare for ch5
  
-  //Enabling interrupt for output compare ch5
-  //TIE_C5I = 1;
- 
   //Enabling PT5 to be output
   DDRT_DDRT5 = 1;
    
@@ -31,14 +33,15 @@ void init_timer(void){
 void delay_ms(int ms){
   /*
     This function uses the timer and the output compare channel 7
-    to create a delay of ms*1 milliseconds (nm loops of 3000 timer
-    counts @8P).
+    to create a delay of 'ms'*1 milliseconds ('ms' loops of 3000 timer
+    counts @8Prescaler).
   */
   int i;
   
   //Loop 1ms timer delay 'ms' times
   for (i = 0; i < ms; i++){
     
+    //add 1millisecond worth of count to current time and store in out/comp7 
     TC7 = TCNT + ONEMS_8;
     
     //keep looping until output compare successful (1ms has passed)
