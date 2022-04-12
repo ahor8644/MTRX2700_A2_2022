46e646d350d968e5d7e550ad41fa4c8913800d10
ahor8644 <101688580+ahor8644@users.noreply.github.com>
2022-04-13 00:29:31 +1000

    Update command_parsing.h

--- MTRX2700_Assignment2/Sources/command_parsing.h
+++ MTRX2700_Assignment2/Sources/command_parsing.h
@@ -4,25 +4,36 @@
 #include "serial.h"
 
 
+/*
+  Module for handling and parsing the user inputted command read in from the SCI
+  (hence dependent on serial.h)
+  Includes a method of splitting the input up into an array of parameter strings
+  and then parsing the command checking for both:
+    - valid, general command layout
+    - function specific parameter validity
+  all done within the general parsing function 'parse_command()'.
+*/
+
+
+//End of read indicator needed by parsing function (signifies end of read in string)
 extern char read_end_char;
 
 
-//Main command parsing function
+//General parsing function - command and function parameter parsing
 int parse_command(SerialPort *serial_port, char **command, int number_of_functions, int number_of_parameters);
 
 
-//Function 1 (flashing function) parser:
+//Function 1 (flashing function) parser: checks function specific parameters: speed, number of flashes and pattern
 int flashing_function_parser(SerialPort *serial_port, char **command, int num_params);
 
-//Function 3 (7-seg num display) parser:
+//Function 3 (7-seg num display) parser: Parses up HEX number (up to 4 digits) to be displayed on 7-segs
 int hex_to_seg_parser(SerialPort *serial_port, char **command, int num_params);
 
 
-
-//splitting up the input into a array of parameter strings
-
+//Splits up the input into an array of parameter strings used for easier parsing
 char **split_up_input(char *input_string, int *number_of_parameters);
 
+//Freeing 2d command/split up input
 void free_str_array(char **str_arr, int num_elements);
 
 
