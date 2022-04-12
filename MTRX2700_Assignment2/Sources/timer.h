13ff4cc00696743750522f162355646db5b9531c
ahor8644 <101688580+ahor8644@users.noreply.github.com>
2022-04-12 20:59:26 +1000

    Update timer.h

--- MTRX2700_Assignment2/Sources/timer.h
+++ MTRX2700_Assignment2/Sources/timer.h
@@ -2,8 +2,13 @@
 #define TIMER_H
 
 
+#define ONEMS_8 3000   //one ms is 3000 counts at 8 prescaler
+
 //enabling timer and TC& output compare (& interrupts)
-void init_TC7(void);
+void init_timer(void);
+
+//Delay function (in 1ms increments)
+void delay_ms(int ms);
 
 
 #endif
